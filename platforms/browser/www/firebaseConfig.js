1644c0cef157af1e9f54c6bf7b033b661bde6779
hiltoncastro <hilton.castrojr@outlook.com>
2019-11-18 22:42:39 -0300

    app

---
+++ platforms/browser/www/firebaseConfig.js
@@ -0,0 +1,13 @@
+
+
+var fbConfig = {
+    apiKey: "AIzaSyBGVs1Vp6OYLb56bCVoEtQ6ZzS5ZpeNVt4",
+    authDomain: "cordovafirebase-1a858.firebaseapp.com",
+    databaseURL: "https://cordovafirebase-1a858.firebaseio.com",
+    projectId: "cordovafirebase-1a858",
+    storageBucket: "cordovafirebase-1a858.appspot.com",
+    messagingSenderId: "514509670108",
+    appId: "1:514509670108:web:19724cb8c92bcc60291619",
+    measurementId: "G-45DC3JGHL4"
+  };
+  
